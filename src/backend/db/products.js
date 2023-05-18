8b4d2bfe03e196019da2588316354ddc9fd9e556
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-05-19 02:02:17 +0530

    Added products details in backend

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -8,23 +8,154 @@ import { v4 as uuid } from "uuid";
 export const products = [
   {
     _id: uuid(),
-    title: "You Can WIN",
-    author: "Shiv Khera",
-    price: "5000",
-    categoryName: "non-fiction",
+    title: "Orange Paithani Silk Dupatta",
+    description: "Handwoven Silk Paithani Dupatta with Zari Bootis all over & Brocade Border of Single Muniya on the Two Long Sides with Pallu in Hastkala Peacockl Motif",
+    price: "28000",
+    categoryName: "dupatta",
+    imageLink: "/images/dupatta/dupatta1.webp"  
   },
   {
     _id: uuid(),
-    title: "You are Winner",
-    author: "Junaid Qureshi",
-    price: "3000",
-    categoryName: "horror",
+    title: "Rani Pink Paithani Silk Dupatta",
+    description: "Handwoven Silk Paithani Dupatta with Zari Bootis all over & Traditional Border in Contrast Colour on the Two Long Sides with Pallu in Hastkala Peacockl Motif",
+    price: "11000",
+    categoryName: "dupatta",
+    imageLink: "/images/dupatta/dupatta2.webp" 
   },
   {
     _id: uuid(),
-    title: "Think and Grow Rich",
-    author: "Shiv Khera",
-    price: "1000",
-    categoryName: "fiction",
+    title: "Magenta Paithani Silk Dupatta",
+    description: "Handwoven Silk Paithani Dupatta with Zari Bootis all over & Traditional Border in Contrast Colour on the Two Long Sides with Pallu in Hastkala Peacockl Motif",
+    price: "14000",
+    categoryName: "dupatta",
+    imageLink: "/images/dupatta/dupatta3.webp" 
   },
+  {
+    _id: uuid(),
+    title: "Violet Paithani Silk Dupatta",
+    description: "Handwoven Silk Paithani Dupatta with Zari Bootis all over & Traditional Border on the Two Long Sides with Pallu in Hastkala Peacockl Motif",
+    price: "16000",
+    categoryName: "dupatta",
+    imageLink: "/images/dupatta/dupatta4.webp" 
+  },
+  {
+    _id: uuid(),
+    title: "Pista Green Paithani Silk Dupatta",
+    description: "Handwoven Silk Paithani Dupatta with Zari Stripes all over & Brocade Border of Single Muniya on the Two Long Sides with Pallu in Hastakala Floral Motif",
+    price: "16000",
+    categoryName: "dupatta",
+    imageLink: "/images/dupatta/dupatta5.webp" 
+  },
+  {
+    _id: uuid(),
+    title: "Rani Pink Paithani Silk Salwar suit",
+    description: "Beautiful suit Jacquard Paithani Salwar suit with Zari Work",
+    price: "2500",
+    categoryName: "kurti",
+    imageLink: "/images/kurti/kurti1.jpg" 
+  },
+  {
+    _id: uuid(),
+    title: "Green Paithani Silk Salwar suit",
+    description: "Paithani Silk Hand Woven Salwar Suit",
+    price: "4200",
+    categoryName: "kurti",
+    imageLink: "/images/kurti/kurti2.jpg" 
+  },
+  {
+    _id: uuid(),
+    title: "Dark Green Paithani Silk Salwar suit",
+    description: "Jacquard Paithani and Zari Work Un-stitched Salwar suit dress",
+    price: "2820",
+    categoryName: "kurti",
+    imageLink: "/images/kurti/kurti3.webp" 
+  },
+  {
+    _id: uuid(),
+    title: "Light Pink Paithani Silk Salwar suit",
+    description: "Paithani and Zari Work Un-stitched Salwar suit dress",
+    price: "3200",
+    categoryName: "kurti",
+    imageLink: "/images/kurti/kurti5.webp" 
+  },
+  {
+    _id: uuid(),
+    title: "Cream White Paithani Lehenga",
+    description: "Paithani Silk Un-stitched lehenga embroidered with floral & cultural motif embellishments and regal flair",
+    price: "29000",
+    categoryName: "lehenga",
+    imageLink: "/images/lehenga/lehenga1.webp" 
+  },
+  {
+    _id: uuid(),
+    title: "Pista Green Paithani Silk Lehenga",
+    description: "Paithani Silk Un-stitched lehenga embroidered with floral & cultural motif embellishments",
+    price: "31300",
+    categoryName: "lehenga",
+    imageLink: "/images/lehenga/lehenga2.webp" 
+  },
+  {
+    _id: uuid(),
+    title: "Orange Kanchipuram Paithani Silk Lehenga Choli",
+    description: "Paithani Silk Weaving Zari Work Un-stitched lehenga embroidered with floral & cultural motif embellishments",
+    price: "33500",
+    categoryName: "lehenga",
+    imageLink: "/images/lehenga/lehenga3.webp" 
+  },
+  {
+    _id: uuid(),
+    title: "Peach Paithani Silk Lehenga",
+    description: "Paithani Silk Un-stitched lehenga embroidered with floral & cultural motif embellishments",
+    price: "26500",
+    categoryName: "lehenga",
+    imageLink: "/images/lehenga/lehenga4.webp" 
+  },
+  {
+    _id: uuid(),
+    title: "Cyan Blue Paithani Upada Silk Lehenga",
+    description: "Paithani Silk Un-stitched lehenga embroidered with floral & cultural motif embellishments",
+    price: "24900",
+    categoryName: "lehenga",
+    imageLink: "/images/lehenga/lehenga5.webp" 
+  },
+  {
+    _id: uuid(),
+    title: "Red Paithani Upada Silk Lehenga",
+    description: "Paithani Silk Un-stitched lehenga embroidered with floral & cultural motif embellishments",
+    price: "30600",
+    categoryName: "lehenga",
+    imageLink: "/images/lehenga/lehenga6.webp" 
+  },
+  {
+    _id: uuid(),
+    title: "Turquoise Green Paithani Silk Saree",
+    description: "Zari Woven Traditional Paithani Silk",
+    price: "6200",
+    categoryName: "saree",
+    imageLink: "/images/saree/saree1.webp" 
+  },
+  {
+    _id: uuid(),
+    title: "Red Paithani Silk Saree",
+    description: "Zari Woven Traditional Paithani Silk",
+    price: "5900",
+    categoryName: "saree",
+    imageLink: "/images/saree/saree2.webp" 
+  },
+  {
+    _id: uuid(),
+    title: "Bottle Green Paithani Silk Saree",
+    description: "Zari Woven Traditional Paithani Silk",
+    price: "5100",
+    categoryName: "saree",
+    imageLink: "/images/saree/saree3.webp" 
+  },
+  {
+    _id: uuid(),
+    title: "Black Paithani Silk Saree",
+    description: "Zari Woven Traditional Paithani Silk",
+    price: "6800",
+    categoryName: "saree",
+    imageLink: "/images/saree/saree4.webp" 
+  }
 ];
