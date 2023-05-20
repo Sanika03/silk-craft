ccb40fa3cc5c849e7e808281836d3c055430deb5
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-05-20 19:02:07 +0530

    Added routes for navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import {Routes, Route} from "react-router-dom"
+
+import { Navigation } from "./components/navigation";
+import { Home } from "./backend/pages/home";
+import { ProductListing } from "./backend/pages/productListing";
+import { Wishlist } from "./backend/pages/wishlist";
+import { Cart } from "./backend/pages/cart";
+import { Profile } from "./backend/pages/profile";
+
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductListing />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
