195fcd839857d4c4dc59a88a53d336933ebdc4c5
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-05-21 21:14:10 +0530

    Added product context

---
+++ src/contexts/productContext.js
@@ -0,0 +1,41 @@
+import { createContext, useEffect, useState } from "react";
+
+export const ProductContext = createContext();
+
+export const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  const [item, setItem] = useState("");
+  const [cartItems, setCartItems] = useState([]);
+  const [wishlistItems, setWishlistItems] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await fetch("api/products")
+      const {products} = await response.json()
+      setProducts(products)
+     }
+    catch(e){
+     console.log(e)
+     }
+  }
+
+  useEffect(() => {
+    getData()
+  }, []);
+
+  return (
+    <ProductContext.Provider
+      value={{
+        products,
+        item,
+        setItem,
+        cartItems,
+        setCartItems,
+        wishlistItems,
+        setWishlistItems
+      }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
+};
