260bfb474554b86b95878bfbcbeed48b02fcaf43
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-05-22 17:16:28 +0530

    Adding rating on the product card

--- src/backend/pages/productListing.js
+++ src/backend/pages/productListing.js
@@ -18,16 +18,17 @@ const {products, setItem, cartItems, setCartItems, wishlistItems, setWishlistIte
   const showProducts = () => (
     <div className="product-container">
         {products.map((product) => {
-          const { id, title, price, imageLink } = product;
+          const { id, title, price, imageLink, rating } = product;
           return (
               <div key={id} onClick={() => handleSingleProductClick(product)} className="product-item">
                 <img src={imageLink} alt={title} className="productImage"/>
-                <p>{title}</p>
-                <p>Rs. {price}</p>
-                <button onClick={() => handleAddToCart(product)} className="cartButton">
+                <p className="text">{title}</p>
+                <p className="text priceText">Rs. {price}</p>
+                <p className="text ratingText">{rating} ⭐</p>
+                <button onClick={() => handleAddToCart(product)} className="button cartButton">
                   Add to Cart
                 </button>
-                <button onClick={() => handleAddToWishlist(product)} className="wishlistButton">
+                <button onClick={() => handleAddToWishlist(product)} className="button wishlistButton">
                 Add to wishlist
                 </button>
               </div>
