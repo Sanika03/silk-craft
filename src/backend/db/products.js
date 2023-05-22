af91644039427219252c0b6c6f1c7de165def52d
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-05-22 16:21:29 +0530

    Added rating to all products

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -12,7 +12,8 @@ export const products = [
     description: "Handwoven Silk Paithani Dupatta with Zari Bootis all over & Brocade Border of Single Muniya on the Two Long Sides with Pallu in Hastkala Peacockl Motif",
     price: "28000",
     categoryName: "dupatta",
-    imageLink: "/images/dupatta/dupatta1.webp"  
+    imageLink: "/images/dupatta/dupatta1.webp",
+    rating: 3.5
   },
   {
     _id: uuid(),
@@ -20,7 +21,8 @@ export const products = [
     description: "Handwoven Silk Paithani Dupatta with Zari Bootis all over & Traditional Border in Contrast Colour on the Two Long Sides with Pallu in Hastkala Peacockl Motif",
     price: "11000",
     categoryName: "dupatta",
-    imageLink: "/images/dupatta/dupatta2.webp" 
+    imageLink: "/images/dupatta/dupatta2.webp",
+    rating: 4.2
   },
   {
     _id: uuid(),
@@ -28,7 +30,8 @@ export const products = [
     description: "Handwoven Silk Paithani Dupatta with Zari Bootis all over & Traditional Border in Contrast Colour on the Two Long Sides with Pallu in Hastkala Peacockl Motif",
     price: "14000",
     categoryName: "dupatta",
-    imageLink: "/images/dupatta/dupatta3.webp" 
+    imageLink: "/images/dupatta/dupatta3.webp",
+    rating: 4.7
   },
   {
     _id: uuid(),
@@ -36,7 +39,8 @@ export const products = [
     description: "Handwoven Silk Paithani Dupatta with Zari Bootis all over & Traditional Border on the Two Long Sides with Pallu in Hastkala Peacockl Motif",
     price: "16000",
     categoryName: "dupatta",
-    imageLink: "/images/dupatta/dupatta4.webp" 
+    imageLink: "/images/dupatta/dupatta4.webp",
+    rating: 2.9
   },
   {
     _id: uuid(),
@@ -44,7 +48,8 @@ export const products = [
     description: "Handwoven Silk Paithani Dupatta with Zari Stripes all over & Brocade Border of Single Muniya on the Two Long Sides with Pallu in Hastakala Floral Motif",
     price: "16000",
     categoryName: "dupatta",
-    imageLink: "/images/dupatta/dupatta5.webp" 
+    imageLink: "/images/dupatta/dupatta5.webp",
+    rating: 3.8
   },
   {
     _id: uuid(),
@@ -52,7 +57,8 @@ export const products = [
     description: "Beautiful suit Jacquard Paithani Salwar suit with Zari Work",
     price: "2500",
     categoryName: "kurti",
-    imageLink: "/images/kurti/kurti1.jpg" 
+    imageLink: "/images/kurti/kurti1.jpg",
+    rating: 4.5
   },
   {
     _id: uuid(),
@@ -60,7 +66,8 @@ export const products = [
     description: "Paithani Silk Hand Woven Salwar Suit",
     price: "4200",
     categoryName: "kurti",
-    imageLink: "/images/kurti/kurti2.jpg" 
+    imageLink: "/images/kurti/kurti2.jpg",
+    rating: 3
   },
   {
     _id: uuid(),
@@ -68,7 +75,8 @@ export const products = [
     description: "Jacquard Paithani and Zari Work Un-stitched Salwar suit dress",
     price: "2820",
     categoryName: "kurti",
-    imageLink: "/images/kurti/kurti3.webp" 
+    imageLink: "/images/kurti/kurti3.webp",
+    rating: 3.6
   },
   {
     _id: uuid(),
@@ -76,7 +84,8 @@ export const products = [
     description: "Paithani and Zari Work Un-stitched Salwar suit dress",
     price: "3200",
     categoryName: "kurti",
-    imageLink: "/images/kurti/kurti5.webp" 
+    imageLink: "/images/kurti/kurti5.webp",
+    rating: 4
   },
   {
     _id: uuid(),
@@ -84,7 +93,8 @@ export const products = [
     description: "Paithani Silk Un-stitched lehenga embroidered with floral & cultural motif embellishments and regal flair",
     price: "29000",
     categoryName: "lehenga",
-    imageLink: "/images/lehenga/lehenga1.webp" 
+    imageLink: "/images/lehenga/lehenga1.webp",
+    rating: 2.9 
   },
   {
     _id: uuid(),
@@ -92,15 +102,8 @@ export const products = [
     description: "Paithani Silk Un-stitched lehenga embroidered with floral & cultural motif embellishments",
     price: "31300",
     categoryName: "lehenga",
-    imageLink: "/images/lehenga/lehenga2.webp" 
-  },
-  {
-    _id: uuid(),
-    title: "Orange Kanchipuram Paithani Silk Lehenga Choli",
-    description: "Paithani Silk Weaving Zari Work Un-stitched lehenga embroidered with floral & cultural motif embellishments",
-    price: "33500",
-    categoryName: "lehenga",
-    imageLink: "/images/lehenga/lehenga3.webp" 
+    imageLink: "/images/lehenga/lehenga2.webp",
+    rating: 4.6
   },
   {
     _id: uuid(),
@@ -108,7 +111,8 @@ export const products = [
     description: "Paithani Silk Un-stitched lehenga embroidered with floral & cultural motif embellishments",
     price: "26500",
     categoryName: "lehenga",
-    imageLink: "/images/lehenga/lehenga4.webp" 
+    imageLink: "/images/lehenga/lehenga4.webp",
+    rating: 4.4 
   },
   {
     _id: uuid(),
@@ -116,7 +120,8 @@ export const products = [
     description: "Paithani Silk Un-stitched lehenga embroidered with floral & cultural motif embellishments",
     price: "24900",
     categoryName: "lehenga",
-    imageLink: "/images/lehenga/lehenga5.webp" 
+    imageLink: "/images/lehenga/lehenga5.webp",
+    rating: 3.3
   },
   {
     _id: uuid(),
@@ -124,7 +129,8 @@ export const products = [
     description: "Paithani Silk Un-stitched lehenga embroidered with floral & cultural motif embellishments",
     price: "30600",
     categoryName: "lehenga",
-    imageLink: "/images/lehenga/lehenga6.webp" 
+    imageLink: "/images/lehenga/lehenga6.webp",
+    rating: 2.8 
   },
   {
     _id: uuid(),
@@ -132,7 +138,8 @@ export const products = [
     description: "Zari Woven Traditional Paithani Silk",
     price: "6200",
     categoryName: "saree",
-    imageLink: "/images/saree/saree1.webp" 
+    imageLink: "/images/saree/saree1.webp",
+    rating: 4.9
   },
   {
     _id: uuid(),
@@ -140,7 +147,8 @@ export const products = [
     description: "Zari Woven Traditional Paithani Silk",
     price: "5900",
     categoryName: "saree",
-    imageLink: "/images/saree/saree2.webp" 
+    imageLink: "/images/saree/saree2.webp",
+    rating: 3.1 
   },
   {
     _id: uuid(),
@@ -148,7 +156,8 @@ export const products = [
     description: "Zari Woven Traditional Paithani Silk",
     price: "5100",
     categoryName: "saree",
-    imageLink: "/images/saree/saree3.webp" 
+    imageLink: "/images/saree/saree3.webp",
+    rating: 4.1 
   },
   {
     _id: uuid(),
@@ -156,6 +165,16 @@ export const products = [
     description: "Zari Woven Traditional Paithani Silk",
     price: "6800",
     categoryName: "saree",
-    imageLink: "/images/saree/saree4.webp" 
-  }
+    imageLink: "/images/saree/saree4.webp",
+    rating: 4.7 
+  },
+  {
+    _id: uuid(),
+    title: "Magenta Katan Silk Paithani Saree",
+    description: "Magenta paithani saree in pure katan silk banarasi handloom base with zari warp, meenakari work and thread work. It is paired with a contrasting blouse piece.",
+    price: "5900",
+    categoryName: "saree",
+    imageLink: "/images/saree/saree5.jpeg",
+    rating: 3.4 
+  },
 ];
