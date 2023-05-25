9fa3ef3fd2038ad18442b9182738fb01b5894d62
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-05-26 00:35:07 +0530

    Added video

--- src/backend/pages/home.js
+++ src/backend/pages/home.js
@@ -1,7 +1,37 @@
+import { useState, useRef } from "react";
+import "../../styles/home.css"
+
 export const Home = () => {
+    const [isHovered, setIsHovered] = useState(false);
+    const videoRef = useRef(null);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+        if (videoRef.current) {
+        videoRef.current.currentTime = 0;
+        videoRef.current.play();
+        }
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+    const getVideo = () => <div className="video-container">
+            <video
+            className={`video ${isHovered ? "hovered" : null}`}
+            src="/images/homeVideo.mp4"
+            autoPlay 
+            muted
+            ref={videoRef}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            ></video>
+        </div>
+
     return (
-        <>
-            
-        </>
+        <div className="mainDiv">
+            {getVideo()}
+        </div>
     )
 }
