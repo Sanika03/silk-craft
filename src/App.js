7451d78fdbe79de93709cd8ed8a453afcb369b32
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-09 02:54:14 +0530

    Changed order of import lines and added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
+import React from "react";
 import {Routes, Route} from "react-router-dom";
+import Mockman from "mockman-js";
 
 import { Navigation } from "./components/navigation";
-import { Home } from "./backend/pages/home";
-import { ProductListing } from "./backend/pages/productListing";
-import { Wishlist } from "./backend/pages/wishlist";
-import { Cart } from "./backend/pages/cart";
-import { Profile } from "./backend/pages/profile";
 import { Footer } from "./components/footer";
-import { ProductDetails } from "./components/singleProduct";
+import { Home } from "./pages/home"
+import { ProductListing } from "./pages/productListing";
+import { Wishlist } from "./pages/wishlist";
+import { Cart } from "./pages/cart";
+import { Profile } from "./pages/profile";
+import { ProductDetails } from "./pages/singleProduct";
+import { Login } from "./pages/login";
+import { SignUp } from "./pages/signUp";
+
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Routes>
+      <Route path="/mockMan" element={<Mockman />} />
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<ProductListing />} />
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/products/:_id" element={<ProductDetails />} />
       </Routes>
       <Footer />
