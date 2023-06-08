a8cef54d942495b316ed46612fe91b6471722040
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-09 02:39:25 +0530

    Added search functionality and navigation link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,30 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faThLarge, faHeart, faShoppingCart, faUser } from "@fortawesome/free-solid-svg-icons";
 
 import "../styles/navigation.css"
+import { useContext } from "react";
+import { FilterContext } from "../contexts/filterContext";
 
 export const Navigation = () => {
+    const {filter, setFilter} = useContext(FilterContext);
+
+    const navigate = useNavigate()
+
+    const handleSearch = (e) => {
+        navigate("/products")
+        setFilter({...filter, search: e.target.value})
+    }
     return (
         <>
             <nav className="navHeader">
-                <div className="titleName">
+                <NavLink to="/" className="titleName">
                     <h1>SilkCraft</h1>
-                </div>
+                </NavLink>
                 <div className="inputDiv">
-                    <input type="search" placeholder="🔍 Search" className="input"></input>
+                    <input type="search" placeholder="🔍 Search" className="input" onChange={(e) => handleSearch(e)}></input>
                 </div>
                 <div className="navDiv">
-                    <NavLink to="/">
-                        <FontAwesomeIcon icon={faHome} className="navIcon" />
-                    </NavLink>
                     <NavLink to="/products">
                         <FontAwesomeIcon icon={faThLarge} className="navIcon" />
                     </NavLink>
