6ab64480a08f2f5f6a9d1fe910354082bd774abe
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-09 02:44:26 +0530

    Added sortby and search properties to filter state

--- src/contexts/filterContext.js
+++ src/contexts/filterContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react"
 export const FilterContext = createContext()
 
 export const FilterProvider = ({children}) => {
-    const [filter, setFilter] = useState({ category: [], rating: 0 });
+    const [filter, setFilter] = useState({ category: [], rating: 0, sortby: '', search: '' });
     return (
         <FilterContext.Provider value={{filter, setFilter}}>
             {children}
