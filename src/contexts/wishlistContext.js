9639810cde2228ce0c57abd2284d2dbf6bd2fd60
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-09 02:42:54 +0530

    Created wishlist handlers and WishlistContext

---
+++ src/contexts/wishlistContext.js
@@ -0,0 +1,60 @@
+import { useEffect, useState, createContext } from "react";
+
+import { useAuth } from "../contexts/authContext";
+import { GetWishList, PostWishList, DeleteWish } from "../services/services";
+
+export const WishlistContext = createContext();
+
+export const WishlistProvider = ({children}) => {
+  const [wishlistItems, setWishlistItems] = useState([]);
+  const { token } = useAuth();
+
+  const getWishlistHandler = async () => {
+    try {
+      const response = await GetWishList({encodedToken: token})
+      if (response.status === 200) {
+        setWishlistItems(response.data.wishlistItems)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const postWishlistHandler = async (item, token) => {
+    try {
+      const response = await PostWishList({
+        product: item,
+        encodedToken: token
+      })
+      if (response.status === 201) {
+        setWishlistItems(response.data.wishlist)
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  const deleteWishlistHandler = async (id, token) => {
+    try {
+      const response = await DeleteWish({
+        productId: id,
+        encodedToken: token
+      })
+      if (response.status === 200) {
+        setWishlistItems(response.data.wishlist)
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  useEffect(() => {
+    getWishlistHandler();
+  }, []);
+
+  return (
+    <WishlistContext.Provider value={{wishlistItems, postWishlistHandler, deleteWishlistHandler}}>
+      {children}
+    </WishlistContext.Provider>
+  )
+}
