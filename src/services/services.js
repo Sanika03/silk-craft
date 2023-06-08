dd8d639c093753c2057712851433d5c5fe859275
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-09 02:54:02 +0530

    Added services for api calls

---
+++ src/services/services.js
@@ -0,0 +1,89 @@
+import axios from 'axios';
+
+export const LoginService = async ({ email, password }) =>{
+  return axios.post('/api/auth/login', {
+    email,
+    password,
+  });
+}
+
+export const SignUpService = async ({ email, password, name }) => {
+  return axios.post('/api/auth/signup', {
+    email,
+    password,
+    name,
+  });
+};
+
+export const GetAllProducts = async () => axios.get('/api/products');
+
+// export const GetAllCategories = async () => axios.get('/api/categories');
+
+// export const GetAllSizes = async () => await axios.get('/api/sizes');
+
+export const GetWishList = async ({ encodedToken }) =>
+  axios.get('/api/user/wishlist', {
+    headers: {
+      authorization: encodedToken,
+    },
+  });
+
+export const PostWishList = async ({ product, encodedToken }) => {
+  return axios.post(
+    '/api/user/wishlist',
+    { product },
+    {
+      headers: {
+        authorization: encodedToken,
+      },
+    }
+  );
+};
+
+export const DeleteWish = async ({ productId, encodedToken }) => {
+  return axios.delete(`/api/user/wishlist/${productId}`, {
+    headers: {
+      authorization: encodedToken,
+    },
+  });
+};
+
+export const GetCartList = async ({ encodedToken }) =>
+  axios.get('/api/user/cart', {
+    headers: {
+      authorization: encodedToken,
+    },
+  });
+
+export const PostCart = async ({ product, encodedToken }) => {
+  return axios.post(
+    '/api/user/cart',
+    { product },
+    {
+      headers: {
+        authorization: encodedToken,
+      },
+    }
+  );
+};
+
+export const IncDecCart = async ({ productId, encodedToken, type }) => {
+  return axios.post(
+    `/api/user/cart/${productId}`,
+    {
+      action: { type },
+    },
+    {
+      headers: {
+        authorization: encodedToken,
+      },
+    }
+  );
+};
+export const DeleteCart = async ({ productId, encodedToken }) => {
+  return axios.delete(`/api/user/cart/${productId}`, {
+    headers: {
+      authorization: encodedToken,
+    },
+  });
+};
