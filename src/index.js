27e2f71fd961817861a1550ee274ad1653ae7128
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-09 02:50:31 +0530

    Added providers

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
+import { makeServer } from "./server";
+import { ScrollToTop } from './components/scrollToTop';
+import { ProductProvider } from "./contexts/productContext";
+import { FilterProvider } from "./contexts/filterContext";
+import { AuthProvider } from "./contexts/authContext";
+import { CartProvider } from "./contexts/cartContext";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ScrollToTop } from './components/scrollToTop';
 
 import App from "./App";
-import { makeServer } from "./server";
-import { ProductProvider } from "./contexts/productContext";
-import { FilterProvider } from "./contexts/filterContext";
+import { WishlistProvider } from "./contexts/wishlistContext";
+
+<link
+  rel="stylesheet"
+  href="https://unpkg.com/mockman-js@latest/dist/style.css"
+/>
 
 makeServer();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <Router>
-            <ScrollToTop />
-                <ProductProvider> 
-                    <FilterProvider>
-                        <App />
-                    </FilterProvider>
-                </ProductProvider>
-        </Router>
-    </React.StrictMode>
+    <Router>
+      <ScrollToTop />
+      <ProductProvider>
+        <FilterProvider>
+          <AuthProvider>
+            <CartProvider>
+              <WishlistProvider>
+                <App />
+              </WishlistProvider>
+            </CartProvider>
+          </AuthProvider>
+        </FilterProvider>
+      </ProductProvider>
+    </Router>
+  </React.StrictMode>
 );
