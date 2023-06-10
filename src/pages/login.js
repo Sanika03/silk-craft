3e9fccb0b8df880254a5bd3d5382ff496a40266d
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-10 12:46:42 +0530

    Refactored login component and changed class names

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,40 +1,57 @@
-import { NavLink, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { NavLink, useNavigate, useLocation } from "react-router-dom";
 // import { toast } from "react-toastify";
+
+import "../styles/login.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 import { useAuth } from "../contexts/authContext";
 
-import "../styles/login.css";
-import { useState } from "react";
-
 export const Login = () => {
+  const location = useLocation();
 
   const testUserData = {
     email: "adarshbalika@gmail.com",
     password: "adarshbalika",
   };
 
-  const { loginHandler } = useAuth();
+  const { loginHandler, token } = useAuth();
 
   const [loginData, setLoginData] = useState({
     email: "",
     password: "",
   });
 
+  const [passwordIsVisible, setPasswordIsVisible] = useState(false);
+
   const navigate = useNavigate();
 
   const handleUserLogin = (e) => {
     e.preventDefault();
     loginHandler(loginData);
-    navigate(-1);
   };
 
+  const togglePasswordVisibility = (e) => {
+    setPasswordIsVisible(!passwordIsVisible);
+    e.preventDefault()
+  };
+
+  useEffect(() => {
+    if (token) {
+      navigate(location?.state?.from.pathname || '/', { replace: true });
+    }
+  }, [token]);
+
   return (
-    <div className="login_component">
-      <form onSubmit={(e) => handleUserLogin(e)} className="login_page">
-        <h3 className="login_head">Sign In</h3>
+    <div className="login-component">
+      <form onSubmit={(e) => handleUserLogin(e)} className="login-page">
+        <h3 className="login-head">Sign In</h3>
         <label>
           Email address{" "}
           <input
+            className="auth-input"
             type="email"
             required
             placeholder="email"
@@ -46,28 +63,41 @@ export const Login = () => {
         </label>
         <label>
           Password{" "}
-          <input
-            type="password"
-            required
-            placeholder="password"
-            value={loginData?.password}
-            onChange={(e) =>
-              setLoginData({ ...loginData, password: e.target.value })
-            }
-          />
+          <div className="password-container">
+            <input
+              className="auth-input"
+              type={passwordIsVisible ? "text" : "password"}
+              required
+              placeholder="password"
+              value={loginData?.password}
+              onChange={(e) =>
+                setLoginData({ ...loginData, password: e.target.value })
+              }
+            />
+            <button
+              type="button"
+              className="toggle-password-btn"
+              onClick={(e) => togglePasswordVisibility(e)}
+            >
+            <FontAwesomeIcon
+              icon={passwordIsVisible ? faEye : faEyeSlash}
+              className="toggle-password-icon"
+            />
+            </button>
+          </div>
         </label>
         <button type="submit" className="login">
           Login
         </button>
         <button
           type="submit"
-          className="login_as_guest"
+          className="login-as-guest"
           onClick={() => setLoginData(testUserData)}
         >
           Login as guest
         </button>
         <p>
-          Don't have an account? <NavLink to="/signUp">Sign up</NavLink>
+          Don't have an account? <NavLink to="/signUp" className="signup-link">Sign up</NavLink>
         </p>
       </form>
     </div>
