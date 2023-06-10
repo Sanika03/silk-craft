dc884a74463967687c01c9d0f24a39cadfcc17d6
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-10 12:44:34 +0530

    Added private route to protected pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import Mockman from "mockman-js";
 
 import { Navigation } from "./components/navigation";
 import { Footer } from "./components/footer";
+import { PrivateRoute } from "./components/privateRoute";
 import { Home } from "./pages/home"
 import { ProductListing } from "./pages/productListing";
+import { ProductDetails } from "./pages/singleProduct";
 import { Wishlist } from "./pages/wishlist";
 import { Cart } from "./pages/cart";
+import { Checkout } from "./pages/checkout";
 import { Profile } from "./pages/profile";
-import { ProductDetails } from "./pages/singleProduct";
 import { Login } from "./pages/login";
 import { SignUp } from "./pages/signUp";
 
@@ -19,13 +21,14 @@ function App() {
     <div className="App">
       <Navigation />
       <Routes>
-      <Route path="/mockMan" element={<Mockman />} />
+        <Route path="/mockMan" element={<Mockman />} />
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<ProductListing />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<PrivateRoute><Wishlist /></PrivateRoute>} />
+        <Route path="/cart" element={<PrivateRoute><Cart /></PrivateRoute>} />
+        <Route path="/checkout" element={<PrivateRoute><Checkout /></PrivateRoute>} />
         <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/products/:_id" element={<ProductDetails />} />
       </Routes>
