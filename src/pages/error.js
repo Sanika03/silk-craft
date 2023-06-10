15048984c1bd9d5be2b9b3eb7d812a9f87bbc47a
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-10 13:25:25 +0530

    Added error page to display when page is not found

--- src/pages/error.js
+++ src/pages/error.js
@@ -1,9 +1,15 @@
-
+import { useNavigate } from "react-router-dom"
+import "../styles/errorPage.css"
 
 export const ErrorPage = () => {
-    return(
-        <>
+    const navigate = useNavigate();
 
-        </>
+    return (
+        <div className="error-page">
+            <img src="/images/PageNotFound.jpg" alt="Page Not Found" className="page-not-found-image"/>
+            <h2 className="not-found-heading">We looked everywhere</h2>
+            <p>Looks like the page you are looking for is missing.</p>
+            <button onClick={() => navigate("/")} className="home-button">Go to Home</button>
+        </div>
     )
 }
