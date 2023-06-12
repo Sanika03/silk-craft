5246ee0e6a6dabc446f19ed45f7376f12dde83f1
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-13 05:03:20 +0530

    Added page to show message on placing an order

---
+++ src/pages/orderSuccessful.js
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router-dom"
+import "../styles/orderSuccessful.css"
+
+export const OrderSuccessful = () => {
+    const navigate = useNavigate();
+    return (
+        <div className="successful-container">
+            <img src="/images/optional/orderSuccessful.png" className="orderSuccessful-image" alt="Order Successful"/>
+            <button onClick={() => navigate("/")} className="home-button">Go to Home</button>
+        </div>
+    )
+}
