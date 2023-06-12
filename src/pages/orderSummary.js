25332e9568af5f9b378b800ca67579bf9bbc0bf0
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-13 05:01:55 +0530

    Added page to show order summary

--- src/pages/orderSummary.js
+++ src/pages/orderSummary.js
@@ -1,7 +1,96 @@
+import { useContext } from "react"
+
+import { AddressContext } from "../contexts/addressContext"
+import { ProductContext } from "../contexts/productContext";
+
+import "../styles/orderSummary.css"
+import { useNavigate } from "react-router-dom";
+
 export const OrderSummary = () => {
+    const { addressData } = useContext(AddressContext);
+    const { products } = useContext(ProductContext);
+
+    const navigate = useNavigate();
+    
+    const cartData = products.filter((el) => el.carted);
+    
+    const getTotalMrp = () => cartData.reduce((acc, curr) => acc + Number(curr.price), 0);
+
+    const getDiscount = () => cartData.reduce((acc, curr) => acc + Number(curr.price - curr.discountedPrice), 0);
+  
+    const getTotalAmount = () => cartData.reduce((acc, curr) => acc + Number(curr.discountedPrice), 0);
+
+    const handlePlaceOrder = () => {
+        navigate("/orderSuccessful")
+    }
+
+    const productDetailsCard = () => <div className="sub-card">
+        <h2 className="products-summary-heading">Product Details</h2>
+        <div className="heading">
+        <p>Product</p>
+        <p>Quantity</p>
+        </div>
+        <div className="summary-products">
+        {cartData?.map((product) => {
+            const { _id, title, imageLink, qty } = product;
+            return (
+            <div key={_id} className="single-summary-product">
+                <div className="summary-product-details">
+                <img src={imageLink} alt={title} className="single-product-image" />
+                <p className="single-product-heading">{title}</p>
+                </div>
+                <p className="product-quantity">{qty}</p>
+            </div>
+            );
+        })}
+        </div>
+    </div>
+  
+    const priceDetailsCard = () => 
+        <div className="summary-price-card">
+          <h3 className="price-card-title">Price Details</h3>
+          <p className="item-text">({cartData.length} item
+          {cartData.length > 1 ? "s" : null})</p>
+              <div className="price-container">
+                <p className="text-price-details">Total MRP</p>
+                <p className="text-price-details">Rs. {getTotalMrp()}</p>
+              </div>
+              <div className="discount-container">
+                <p className="text-price-details">Discount on MRP</p>
+                <p className="text-price-details">- Rs. {getDiscount()}</p>
+              </div>
+              <div className="delivery-container">
+              <p>Delivery Charges</p>
+              <p>FREE</p>
+              </div>
+              <div className="total-price-container">
+                <p className="text-price-details">Total Amount</p>
+                <p className="text-price-details">Rs. {getTotalAmount()}</p>
+              </div>
+        </div>
+
+    const deliveryDetailsCard = () => (
+        <div className="delivery-details-card">
+        <h2 className="delivery-details-heading">Delivery Details</h2>
+        <h3>{addressData.selectedAddress.name}</h3>
+        <p>{Object.values(addressData.selectedAddress).slice(1).join(", ")}</p>
+        </div>
+    );
+
+    const getSummaryCard = () => <div className="order-summary-card">
+        {productDetailsCard()}
+        <hr/>
+        {priceDetailsCard()}
+        <hr/>
+        {deliveryDetailsCard()}
+        <hr/>
+        <button className="place-order-button" onClick={() => handlePlaceOrder()}>Place Order</button>
+    </div>
+
     return(
-        <div>
-            
+        <div className="summary-page">
+            <h1 className="summary-heading">Order Summary</h1>
+            {getSummaryCard()}
         </div>
     )
 }
