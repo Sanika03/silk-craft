c8365503220a026e85691bbe6bcdd82671dd08ce
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-15 03:23:40 +0530

    Added toast and refactored return of component

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { NavLink, useNavigate, useLocation } from "react-router-dom";
-// import { toast } from "react-toastify";
+import { toast } from "react-toastify";
 
 import "../styles/login.css";
 
@@ -29,6 +29,7 @@ export const Login = () => {
   const navigate = useNavigate();
 
   const handleUserLogin = (e) => {
+    toast.success('Successfully Logged In!');
     e.preventDefault();
     loginHandler(loginData);
   };
@@ -44,62 +45,66 @@ export const Login = () => {
     }
   }, [token]);
 
-  return (
-    <div className="login-component">
-      <form onSubmit={(e) => handleUserLogin(e)} className="login-page">
-        <h3 className="login-head">Sign In</h3>
-        <label>
-          Email address{" "}
+  const loginPage = () => <div className="login-component">
+    <form onSubmit={(e) => handleUserLogin(e)} className="login-page">
+      <h3 className="login-head">Sign In</h3>
+      <label>
+        Email address{" "}
+        <input
+          className="auth-input"
+          type="email"
+          required
+          placeholder="email"
+          value={loginData?.email}
+          onChange={(e) =>
+            setLoginData({ ...loginData, email: e.target.value })
+          }
+        />
+      </label>
+      <label>
+        Password{" "}
+        <div className="password-container">
           <input
             className="auth-input"
-            type="email"
+            type={passwordIsVisible ? "text" : "password"}
             required
-            placeholder="email"
-            value={loginData?.email}
+            placeholder="password"
+            value={loginData?.password}
             onChange={(e) =>
-              setLoginData({ ...loginData, email: e.target.value })
+              setLoginData({ ...loginData, password: e.target.value })
             }
           />
-        </label>
-        <label>
-          Password{" "}
-          <div className="password-container">
-            <input
-              className="auth-input"
-              type={passwordIsVisible ? "text" : "password"}
-              required
-              placeholder="password"
-              value={loginData?.password}
-              onChange={(e) =>
-                setLoginData({ ...loginData, password: e.target.value })
-              }
-            />
-            <button
-              type="button"
-              className="toggle-password-btn"
-              onClick={(e) => togglePasswordVisibility(e)}
-            >
-            <FontAwesomeIcon
-              icon={passwordIsVisible ? faEye : faEyeSlash}
-              className="toggle-password-icon"
-            />
-            </button>
-          </div>
-        </label>
-        <button type="submit" className="login">
-          Login
-        </button>
-        <button
-          type="submit"
-          className="login-as-guest"
-          onClick={() => setLoginData(testUserData)}
-        >
-          Login as guest
-        </button>
-        <p>
-          Don't have an account? <NavLink to="/signUp" className="signup-link">Sign up</NavLink>
-        </p>
-      </form>
-    </div>
+          <button
+            type="button"
+            className="toggle-password-btn"
+            onClick={(e) => togglePasswordVisibility(e)}
+          >
+          <FontAwesomeIcon
+            icon={passwordIsVisible ? faEye : faEyeSlash}
+            className="toggle-password-icon"
+          />
+          </button>
+        </div>
+      </label>
+      <button type="submit" className="login">
+        Login
+      </button>
+      <button
+        type="submit"
+        className="login-as-guest"
+        onClick={() => setLoginData(testUserData)}
+      >
+        Login as guest
+      </button>
+      <p>
+        Don't have an account? <NavLink to="/signUp" className="signup-link">Sign up</NavLink>
+      </p>
+    </form>
+  </div>
+
+  return (
+    <>
+      {loginPage()}
+    </>
   );
 };
