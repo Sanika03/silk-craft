48904e023103a99928d8cd4277ddfa9bb8694d81
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-07-26 08:41:42 +0530

    Change server's logging to false

--- src/server.js
+++ src/server.js
@@ -43,7 +43,7 @@ export function makeServer({ environment = "development" } = {}) {
     // Runs on the start of the server
     seeds(server) {
       // disballing console logs from Mirage
-      server.logging = true;
+      server.logging = false;
       products.forEach((item) => {
         server.create("product", { ...item });
       });
